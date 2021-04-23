0c75d31561b0701576bd9add1974eaf259e11b42
ADITYA RASTOGI <56511496+adityarastogi1212@users.noreply.github.com>
2021-04-23 17:54:37 +0530

    Update register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { WebService } from '../WebService';
 
@@ -30,6 +30,21 @@ export class RegisterComponent implements OnInit {
     this.api.retriveRegister(this.userData).subscribe(response=>this.data=response);
   }
 
+  websiteList: any = ['ItSolutionStuff.com', 'HDTuto.com', 'Nicesnippets.com']
+  
+  form = new FormGroup({
+    website: new FormControl('', Validators.required)
+  });
+  
+  get f(){
+    return this.form.controls;
+  }
+  
+  submit(){
+    console.log(this.form.value);
+  }
+  
+
   submitData2() {
 
     this.demoname=this.retrieveUsername2();
@@ -37,6 +52,16 @@ export class RegisterComponent implements OnInit {
       this.api.retriveRegister(this.userData).subscribe(response=>this.data=response);
       this.flag=1;
       // setTimeout(() => this.router.navigate(['login']),2000);
+      // setTimeout(() => {
+      //   if(this.data.status==true){
+      //       //this.name = this.userData.value.username;
+      //       console.log(this.retrieveUsername2());
+      //       if(confirm("Registered Successfully")) {}
+      //     }
+      //   else{
+      //       if(confirm("User Exists")) {}
+      //     }
+      //   },2000);
     }
     
   }
